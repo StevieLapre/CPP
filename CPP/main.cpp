a0a42c91ae9289df4d5971afdae0c589920a794c
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-01-30 15:00:42 +0100

    basic data storage variables

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -2,15 +2,16 @@
 
 using namespace std;
 
+//Basic data storage in strings
 int main()
 {
     string animal1 = "Fire Rabbit";
     string animal2 = " Wood Dragon";
     string animal3 = "Wood Snake";
 
-    cout << "This years zodiac animal(2024) is the Wood Dragon" << endl;
-    cout << "The previous years zodiac animal(2023) is the Fire Rabbit" << endl;
-    cout << "The next years zodiac animal(2025) is the Wood Snake" << endl;
+    cout << "This years zodiac animal(2024) is the "<< animal2 << endl;
+    cout << "The previous years zodiac animal(2023) is the "<< animal1 << endl;
+    cout << "The next years zodiac animal(2025) is the "<< animal3 << endl;
 
     return 0;
 }
