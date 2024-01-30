00a12b0076c2c0bc2405b8c42798f1444d296cb9
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-01-30 14:55:46 +0100

    basic data storage example 1

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World 123!" << endl;
-    cout << "This is C++ in VSCODE" << endl;
+    string animal1 = "Fire Rabbit";
+    string animal2 = " Wood Dragon";
+    string animal3 = "Wood Snake";
+
+    cout << "This years zodiac animal(2024) is the Wood Dragon" << endl;
+    cout << "The previous years zodiac animal(2023) is the Fire Rabbit" << endl;
+    cout << "The next years zodiac animal(2025) is the Wood Snake" << endl;
+
     return 0;
 }
