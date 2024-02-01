362a02c0ecf860da213dd3a2acdec933fd1d0c49
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-01 14:33:32 +0100

    change inside variable

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -7,9 +7,10 @@ int main()
 {
     string nummer1 = "Een simpele zin";
 double grootte1 = nummer1.size();
-int vind1 = nummer1.find("sim", 0);
+int vind1 = nummer1.find("zin", 0);
+nummer1[0]='g';
 
-    cout << vind1 << endl;
+    cout << nummer1 << endl;
 
     return 0;
 }
