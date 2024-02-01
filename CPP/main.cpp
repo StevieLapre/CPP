30ceddb16a0c23fa833c2a8fb6b1fc4f2764be7b
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-01 15:48:05 +0100

    Clear the function

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -9,8 +9,9 @@ int main()
 double grootte1 = nummer1.size();
 int vind1 = nummer1.find("zin", 0);
 nummer1[0]='g';
+nummer1.clear();
 
-    cout << nummer1 << endl;
+cout << nummer1 << endl;
 
     return 0;
 }
