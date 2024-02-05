8493f600c7c983b9a8ad74e418a22dcf466f3224
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-05 10:31:09 +0100

    prompting to the user

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <string> 
 
 using namespace std;
 
 //practice assignment block 1
 int main()
 {
-    string name = "Xavi";
-    int food1 = 4;
-    int food2 = 8;
-    string oef1 = "gesport";
-    int oef2 = 30;
-    int food3 = 320;
-    int cal1 = 560;
-    
-    cout <<"Hello " << name << endl;
-    cout <<"Ik heb " << min(food1, food2) << " stukken pizza gegeten en ze kosten " << max(food1, food2) << " euro totaal." << endl;
-    cout << "Ik heb " << oef1 << " voor " << min(oef2, food3) << " minuten en ik heb " << max(oef2, food3) << " calorieen verbrand." << endl;
-    cout << "ik heb met dit totaal " << cal1-food3 << " calorieen ingenomen" << endl;
+    cout << "Hoi, waar heb je zin in om vandaag te eten?:";
+    string ant1;
+    getline(cin,ant1);
+    cout << "Je keuze voor het eten vandaag is " << ant1 << endl;
     return 0;
 }
