33876ff408951b3d311ee3a26559a279df52b889
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-05 12:26:50 +0100

    arrays

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -2,24 +2,14 @@
 #include <cmath>
 #include <algorithm>
 #include <string> 
+#include <vector> 
 
 using namespace std;
 
-//building a basic food ordering app
+//storing more data part 1
 int main()
 {
-  cout << "Welkom bij de voedsel order app!" << endl;
-  cout << "Wat zou je willen bestellen?: " << endl;
-  string best1;
-  getline(cin, best1);
-  cout << "Wat zou je erbij willen drinken?: " << endl;
-  string best2;
-  getline(cin, best2);      
-    cout << "wat is de totale prijs van je " << best1 << "?" << endl;
-    double price1;
-    cin >> price1;
-    cout << "wat is de totale prijs van je " << best2 << "?" << endl;
-    double price2;
-    cin >> price2;
-    cout << "Je hebt besteld: " << best1 << " en " << best2 << ". De totale prijs van je bestelling is: " << price1 + price2 << ",- euro" << endl;
+    string voedsel[] = {"pizza", "pasta", "burger", "frietjes", "salade", "soep", "toetje"}; 
+    cout << voedsel[7] << endl;
+    return 0;
 }
