d8a735b1a5438eb806a72f468bf7e9e1f8995d2b
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-05 12:30:32 +0100

    filling up the array by yourself

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -9,7 +9,15 @@ using namespace std;
 //storing more data part 1
 int main()
 {
-    string voedsel[] = {"pizza", "pasta", "burger", "frietjes", "salade", "soep", "toetje"}; 
-    cout << voedsel[7] << endl;
+    string voedsel[8] = {}; 
+    voedsel[0] = "Brood";
+    voedsel[1] = "Kaas";
+    voedsel[2] = "Melk";
+    voedsel[3] = "Eieren"; 
+    voedsel[4] = "Boter";
+    voedsel[5] = "Yoghurt";
+
+
+    cout << voedsel[3] << endl;
     return 0;
 }
