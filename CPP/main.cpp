aa412c1b69ef353ed78743d1fb5651a07210492f
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-06 15:12:25 +0100

    multiple calculations inside of functions

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -8,13 +8,16 @@ using namespace std;
 
 //workin with multiple functions part 2
 
-int calc(int calc1, int calc2){
-    cout << calc1+calc2 << endl;
+int calc(int calc1, int calc2, int calc3, int calc4){
+    cout << calc1+calc2+calc3+calc4 << endl;
+    cout << calc1-calc2-calc3-calc4 << endl;
+    cout << calc1*calc2*calc3*calc4 << endl;
+    cout << calc1/calc2/calc3/calc4 << endl;
     return 0;
 }
 
 
 int main() {
-    calc(30, 76);
+    calc(1, 2, 3, 4 );
     return 0;
  }
