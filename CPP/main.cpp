3042613a35f2556234b8413dd597b68461fdaee4
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-06 14:36:28 +0100

    function inside function called correctly

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -8,17 +8,20 @@ using namespace std;
 
 //workin with multiple functions part 1
 int food(){
+
     string food[4]={};
+
     food[0] = "Pizza";
     food[1] = "Burger";
     food[2] = "Pasta";
     food[3] = "Fries";
 
-    cout << food[0] << endl;
-}
+    cout << food[1] << endl;
 
-int main()
-{
-    
     return 0;
 }
+
+int main() {
+    food();
+    return 0;
+ }
