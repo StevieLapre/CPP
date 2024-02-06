2f6890e1c5e36597fb646ddfeaa34d0ab5d19904
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-06 15:18:40 +0100

    prompting for values to the user inside functions

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -18,11 +18,15 @@ int calc(int calc1, int calc2, int calc3, int calc4){
 
 
 int main() {
-    int out1 = 1;
-    int out2 = 2;
-    int out3 = 3;
-    int out4 = 4;
-    
+    int out1;
+    cin>>out1;
+    int out2;
+    cin>>out2;
+    int out3;
+    cin>>out3;
+    int out4;
+    cin>>out4;
+
     calc(out1, out2, out3, out4);
     return 0;
  }
