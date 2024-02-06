758e3fae11330264d3b362f4a22e1aabc0664f41
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-06 14:55:17 +0100

    calling the function of different sets functions inside a main function

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -10,18 +10,24 @@ using namespace std;
 int food(){
 
     string food[4]={};
-
     food[0] = "Pizza";
     food[1] = "Burger";
     food[2] = "Pasta";
-    food[3] = "Fries";
+    food[3] = "Taco's";
 
-    cout << food[1] << endl;
+    int pr_food[4] = {};
+    pr_food[0] = 1;
+    pr_food[1] = 2;
+    pr_food[2] = 3;
+    pr_food[3] = 4;
+    
+    cout << pr_food[0]+pr_food[1]+pr_food[2]+pr_food[3] << endl;
 
     return 0;
 }
 
 int main() {
+    cout << "De totale proties die aangeboden kunnen worden zijn:" << endl;
     food();
     return 0;
  }
