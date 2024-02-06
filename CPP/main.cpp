3342379b30983799fe691b1ae4e79a7607e79900
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-06 15:34:03 +0100

    understandung various C++ operators

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -6,27 +6,64 @@
 
 using namespace std;
 
-//workin with multiple functions part 2
-
-int calc(int calc1, int calc2, int calc3, int calc4){
-    cout << calc1+calc2+calc3+calc4 << endl;
-    cout << calc1-calc2-calc3-calc4 << endl;
-    cout << calc1*calc2*calc3*calc4 << endl;
-    cout << calc1/calc2/calc3/calc4 << endl;
-    return 0;
-}
-
-
+//understanding various C++ operators
 int main() {
-    int out1;
-    cin>>out1;
-    int out2;
-    cin>>out2;
-    int out3;
-    cin>>out3;
-    int out4;
-    cin>>out4;
+    //comapring operators
+   a == b : //a is equal to b
+   a != b : //a is not equal to b
+   a > b : //a is greater than b
+   a < b : //a is smaller than b
+   a>= b : //a is greater than or equal to b
+   a<= b : //a is smaller than or equal to b
 
-    calc(out1, out2, out3, out4);
+    //logical operators
+    a == b && a<5 : //return true if the two conditions are met
+    a<b || a<c : //return true if at least one of the conditions is met
+    !(a<b) : //return false if the result is 'true' and true if the result is 'false'
+
+
+    //arithmetic operators
+    a+b : //addition
+    a-b : //subtraction
+    a*b : //multiplication
+    a/b : //division
+    a%b : //modulus
+    a++ : //increment
+    a-- : //decrement
+    
+    //assignment operators
+    a = b : //assigns the value of b to a
+    a += b : //adds the value of b to a
+    a -= b : //subtracts the value of b from a
+    a *= b : //multiplies the value of a by b
+    a /= b : //divides the value of a by b
+    a %= b : //assigns the remainder of a divided by b to a
+    a &= b : //assigns a to a & b
+    a |= b : //assigns a to a | b
+    a ^= b : //assigns a to a ^ b
+    a >>= b : //assigns a to a >> b
+    a <<= b : //assigns a to a << b
+    
+    //bitwise operators
+    a & b : //bitwise AND
+    a | b : //bitwise OR
+    a ^ b : //bitwise XOR
+    ~a : //bitwise NOT
+    a << b : //bitwise left shift
+    a >> b : //bitwise right shift
+    
+    //conditional operators
+    a > b ? a : b : //if a is greater than b, return a, else return b
+    a < b ? a : b : //if a is smaller than b, return a, else return b
+    
+    //sizeof operator
+    sizeof(int) : //returns the size of an integer
+    sizeof(a) : //returns the size of the variable a
+    sizeof a : //returns the size of the variable a
+
+    //comma operator
+    a, b : //evaluates a and b and returns the value of b
+    a, b, c : //evaluates a, b and c and returns the value of c
+
     return 0;
  }
