32d6db4abdd255ce5332751d27b60404d5b207c5
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2024-02-08 16:56:41 +0100

    color application with switch statement

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -6,24 +6,43 @@
 
 using namespace std;
 
-//understanding loops part 2
+//generating different colors with a switch statement
+string kleur(int kleur){
+    string color;
+
+    switch(kleur){
+        case 1:
+            color = "rood";
+            break;
+        case 2:
+            color = "blauw";
+            break;
+        case 3:
+            color = "groen";
+            break;
+        case 4:
+            color = "geel";
+            break;
+        case 5:
+            color = "paars";
+            break;
+        case 6:
+            color = "oranje";
+            break;
+        case 7:
+            color = "zwart";
+            break;
+        case 8:
+            color = "wit";
+            break;
+        default:
+            color = "kleur onbekend";
+            break;
+    }
+    return color;
+}
+
 int main() {
-
-cout << "Geef een wachtwoord op: ";
-string ww;
-getline(cin, ww);
-
-cout << "Geef het wachtwoord nogmaals op: ";
-string ww2;
-getline(cin, ww2);
-
-while (ww != ww2) {
-    cout << "De wachtwoorden komen niet overeen, probeer het opnieuw: ";
-    getline(cin, ww2);
-}   cout << "U bent succesvol ingelogd!" << endl;
-
-//while loop
-int i = 0;
-
+    cout << kleur(10) << endl;
 return 0;
 }
